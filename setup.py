253ee72f2051babf2d9062c794a3e1bc26f0e4aa
tadashi-aikawa <syou.maman@gmail.com>
2016-11-18 17:16:37 +0900

    Troubleshooting problems that typings modules can not install

--- setup.py
+++ setup.py
@@ -2,25 +2,16 @@
 # coding: utf-8
 
 from setuptools import setup, find_packages
-from dictmixin import __author__, __version__, __license__
-
-
-def parse_requirements(requirements):
-    with open(requirements) as f:
-        return [l.strip('\n') for l in f if l.strip('\n') and not l.startswith('#')]
-
-
-reqs = parse_requirements("requirements.txt")
 
 setup(
     name='dictmixin',
-    version=__version__,
+    version='0.1.0',
     description='Parsing mixin which converts `data class instance`, `dict object`, and `json string` each other.',
-    license=__license__,
-    author=__author__,
+    license='MIT',
+    author='Tadashi Aikawa',
     author_email='syou.maman@gmail.com',
     url='https://github.com/tadashi-aikawa/dictmixin.git',
     keywords='dict json convert parse each other',
     packages=find_packages(),
-    install_requires=reqs
+    install_requires=[]
 )
