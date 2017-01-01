070dbd4aed9e2bdd447361959a82f8410dafabf4
tadashi-aikawa <syou.maman@gmail.com>
2017-01-01 20:22:36 +0900

    :recycle: Modify files

--- release.sh
+++ release.sh
@@ -3,5 +3,6 @@
 python setup.py bdist_wheel
 twine upload dist/dictmixin-${RELEASE_VERSION}-py2.py3-none-any.whl \
   --repository-url "https://pypi.python.org/pypi" \
+  --config-file "./pypirc" \
   -u tadashi-aikawa \
   -p ${PYPI_PASSWORD}
