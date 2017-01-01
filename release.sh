f164674813a42dee9b1cd8b9f33cdf575758ba00
tadashi-aikawa <syou.maman@gmail.com>
2017-01-01 19:17:15 +0900

    :recycle: Fix wrong option

--- release.sh
+++ release.sh
@@ -4,4 +4,4 @@ readonly RELEASE_VERSION=$1
 readonly PYPI_PASSWORD=$2
 
 python setup.py bdist_wheel
-twine upload dist/dictmixin-${RELEASE_VERSION}-py2.py3-none-any.whl -P ${PYPI_PASSWORD}
+twine upload dist/dictmixin-${RELEASE_VERSION}-py2.py3-none-any.whl -p ${PYPI_PASSWORD}
