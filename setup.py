4552e1c97c44ff851952d30badab4f07b6d647b0
tadashi-aikawa <syou.maman@gmail.com>
2017-03-05 23:41:15 +0900

    :package: Version 1.0.0

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
         'test': ['pytest', 'pytest-cov']
     },
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Utilities',
