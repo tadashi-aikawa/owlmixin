864a625b1775b5fbfa977822d0569be372ab89e1
tadashi-aikawa <syou.maman@gmail.com>
2017-06-07 23:08:12 +0900

    :package: Version 2.0.0a3

--- owlmixin/__init__.py
+++ owlmixin/__init__.py
@@ -8,7 +8,7 @@ from owlmixin.owlenum import OwlEnum, OwlObjectEnum
 from owlmixin import util
 from owlmixin.transformers import DictTransformer, JsonTransformer, YamlTransformer, traverse_dict, TOption
 
-__version__ = '2.0.0a2'
+__version__ = '2.0.0a3'
 
 T = TypeVar('T', bound='OwlMixin')
 
