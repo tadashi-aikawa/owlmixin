23bd81ff180a09eca205c34e6932d3d3c13d2186
tadashi-aikawa <syou.maman@gmail.com>
2017-07-20 10:21:12 +0900

    Fix

--- release.sh
+++ release.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 python setup.py bdist_wheel
-twine upload dist/owlmixin-${RELEASE_VERSION}-py2.py3-none-any.whl \
+twine upload dist/owlmixin-${RELEASE_VERSION}-py3-none-any.whl \
   --config-file ".pypirc" \
   -u tadashi-aikawa \
   -p ${PYPI_PASSWORD}
