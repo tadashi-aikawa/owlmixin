564ff0cb0bfcc027d5e93f407138fa6b00b7c193
tadashi-aikawa <syou.maman@gmail.com>
2017-07-20 09:14:01 +0900

    :hammer: Remove legacy url

--- release.sh
+++ release.sh
@@ -2,7 +2,6 @@
 
 python setup.py bdist_wheel
 twine upload dist/owlmixin-${RELEASE_VERSION}-py2.py3-none-any.whl \
-  --repository-url "https://pypi.python.org/pypi" \
   --config-file ".pypirc" \
   -u tadashi-aikawa \
   -p ${PYPI_PASSWORD}
